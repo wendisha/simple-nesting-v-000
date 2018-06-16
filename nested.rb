7331f6c180927935714fb18733c70f897f820aae
Wendy Calderon <wendycalderon@gmail.com>
2018-06-16 03:12:35 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -19,6 +19,8 @@ def hopper
   programmer_hash[:grace_hopper]
 end
 
+
+
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
   programmer_hash [:alan_kay]	[:known_for]
