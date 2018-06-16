4bba3f7829abce4678deaf03c37386abc41ddd57
Wendy Calderon <wendycalderon@gmail.com>
2018-06-16 03:15:01 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages]
+  dennis_language = programmer_hash[:dennis_ritchie][:languages]
+  dennis_ritchie.split
 end
 
 
