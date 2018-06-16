bc6fcb166d47084b0933893567b6e56edaaa6bf4
Wendy Calderon <wendycalderon@gmail.com>
2018-06-16 03:21:06 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:yukihiro_matsumoto => [:known_for] = "Ruby"]
+  programmer_hash[:yukihiro_matsumoto]
     
 end
 
