cdb84e3afad12ad55fad442d4143e29b5ce5387c
Wendy Calderon <wendycalderon@gmail.com>
2018-06-16 03:12:50 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -23,7 +23,6 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-  programmer_hash [:alan_kay]	[:known_for]
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -39,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  programmer_hash [:alan_kay]	[:known_for]
 end
 
 def dennis_ritchies_language
