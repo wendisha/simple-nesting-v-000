22d5859074b3c7ffa44c4df2614800567d15c172
Wendy Calderon <wendycalderon@gmail.com>
2018-06-16 04:33:54 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -88,9 +88,7 @@ def adding_matz
         }
      }
   letsee = 
-  programmer_hash [:yukihiro_matsumoto] = {}
-  programmer_hash [:yukihiro_matsumoto] :known_for
-  programmer_hash [:yukihiro_matsumoto] = :languages
+  programmer_hash [:yukihiro_matsumoto] = {:known_for, :languages}
   programmer_hash [:yukihiro_matsumoto][:known_for] = "Ruby"
   programmer_hash [:yukihiro_matsumoto][:languages] << ["LISP", "C"]
   programmer_hash  
